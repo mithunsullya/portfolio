0d7f4c2ba8479eacac708cd0d728eb2dbc7dbd9d
Mithun <mithun@specbee.com>
2021-03-06 14:05:11 +0530

    Updates

---
+++ precache-manifest.aa4d858a31fa03af96c6877e79f0d04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cea61c35b54ecd9e1a21259243b0f5a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e830a2aed611f028cdb3",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "c9a596b8baeaf8c7e085",
+    "url": "/portfolio/static/js/2.8a91fe70.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/portfolio/static/js/2.8a91fe70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e830a2aed611f028cdb3",
+    "url": "/portfolio/static/js/main.e3f750cf.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
